db988a1a817698467904ab6c05206dd8302a90d1
KhanhNguyen <khanhnguyen@wee.vn>
2020-10-27 17:37:32 +0700

    add image slider

--- app/src/main/java/wee/digital/fpa/ui/adv/AdvFragment.kt
+++ app/src/main/java/wee/digital/fpa/ui/adv/AdvFragment.kt
@@ -6,12 +6,15 @@ import kotlinx.android.synthetic.main.adv.*
 import wee.digital.fpa.R
 import wee.digital.fpa.ui.base.BaseDialog
 import wee.digital.fpa.ui.base.activityVM
+
 class AdvFragment : BaseDialog() {
 
     private val videoList = listOf(
             MyMediaPlayer(RawResourceDataSource.buildRawResourceUri(R.raw.video_tree).toString()),
             MyMediaPlayer(RawResourceDataSource.buildRawResourceUri(R.raw.video_water).toString())
     )
+
+    private val imageList = listOf(R.mipmap.img_adv1, R.mipmap.img_adv2, R.mipmap.img_adv3, R.mipmap.img_adv4)
 
     private lateinit var adapter: MediaAdapter
 
@@ -22,10 +25,11 @@ class AdvFragment : BaseDialog() {
     }
 
     override fun onViewCreated() {
-        adapter = MediaAdapter().also {
-            it.set(videoList)
-            it.bindToViewPager(advertiseViewPager)
-        }
+//        adapter = MediaAdapter().also {
+//            it.set(videoList)
+//            it.bindToViewPager(advertiseViewPager)
+//        }
+        advImageSlider.listItem = imageList
     }
 
     override fun onLiveDataObserve() {
