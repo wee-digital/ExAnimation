a0653f80cb39ed25f56b30182fb043b570a378d6
KhanhNguyen <khanhnguyen@wee.vn>
2020-10-27 09:45:59 +0700

    update media slider

--- app/src/main/java/wee/digital/fpa/ui/adv/AdvFragment.kt
+++ app/src/main/java/wee/digital/fpa/ui/adv/AdvFragment.kt
@@ -1,11 +1,20 @@
 package wee.digital.fpa.ui.adv
 
 import android.view.View
+import kotlinx.android.synthetic.main.adv.*
 import wee.digital.fpa.R
 import wee.digital.fpa.ui.base.BaseDialog
 import wee.digital.fpa.ui.base.activityVM
-
 class AdvFragment : BaseDialog() {
+
+    //Sample test
+    private val videoList = listOf(
+            MyMediaPlayer("https://assets.mixkit.co/videos/preview/mixkit-a-woman-walking-on-the-beach-on-a-sunny-day-1208-large.mp4"),
+            MyMediaPlayer("https://assets.mixkit.co/videos/preview/mixkit-tree-with-yellow-flowers-1173-large.mp4"),
+            MyMediaPlayer("https://assets.mixkit.co/videos/preview/mixkit-woman-swimming-in-a-pool-1258-large.mp4")
+    )
+
+    private lateinit var adapter: MediaAdapter
 
     private val vm by lazy { activityVM(AdvVM::class) }
 
@@ -14,7 +23,10 @@ class AdvFragment : BaseDialog() {
     }
 
     override fun onViewCreated() {
-
+        adapter = MediaAdapter().also {
+            it.set(videoList)
+            it.bindToViewPager(advertiseViewPager)
+        }
     }
 
     override fun onLiveDataObserve() {
---
+++ app/src/main/java/wee/digital/fpa/ui/adv/MyMediaPlayer.kt
@@ -0,0 +1,6 @@
+package wee.digital.fpa.ui.adv
+
+
+class MyMediaPlayer (val uri: String) {
+    var myMediaPlayer: MyMediaController? = null
+}
