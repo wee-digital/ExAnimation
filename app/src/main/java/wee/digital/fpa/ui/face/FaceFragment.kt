b7770b01f722cb853b69d6dc6b3d1ba5601af56c
Huy Quoc Vu <huyquocvu.sg@gmail.com>
2020-10-29 11:42:42 +0700

    commit

--- app/src/main/java/wee/digital/fpa/ui/face/FaceFragment.kt
+++ app/src/main/java/wee/digital/fpa/ui/face/FaceFragment.kt
@@ -9,7 +9,7 @@ import wee.digital.fpa.ui.base.activityVM
 import wee.digital.fpa.ui.confirm.ConfirmVM
 
 class FaceFragment : Main.Fragment() {
-    
+
     private val faceVM: FaceVM by lazy { viewModel(FaceVM::class) }
 
     private val faceView: FaceView by lazy { FaceView(this) }
