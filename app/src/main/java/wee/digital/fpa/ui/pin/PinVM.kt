1e2095c0bd975446d1a03a9757df7ff82230d60d
Huy Quoc Vu <huyquocvu.sg@gmail.com>
2020-10-31 09:50:26 +0700

    commit

--- app/src/main/java/wee/digital/fpa/ui/pin/PinVM.kt
+++ app/src/main/java/wee/digital/fpa/ui/pin/PinVM.kt
@@ -4,9 +4,8 @@ import android.util.Base64
 import androidx.lifecycle.MutableLiveData
 import crypto.Crypto
 import wee.digital.fpa.data.local.Config
-import wee.digital.fpa.repository.dto.FaceArg
+import wee.digital.fpa.repository.dto.*
 import wee.digital.fpa.repository.dto.PinArg
-import wee.digital.fpa.repository.dto.VerifyPINCodeDTOReq
 import wee.digital.fpa.repository.network.Api
 import wee.digital.fpa.repository.payment.PaymentRepository
 import wee.digital.fpa.ui.Event
@@ -31,25 +30,29 @@ class PinVM : BaseViewModel() {
 
     val errorMessage = EventLiveData<MessageArg>()
 
+    val paymentResult = EventLiveData<Boolean>()
+
+
     fun onPinFilled(pinCode: String, paymentArg: PaymentArg?, faceArg: FaceArg?) {
         paymentArg ?: throw Event.paymentArgError
         faceArg ?: throw Event.faceArgError
         val hashCode = Crypto.hash(pinCode)
         val finalCode = Base64.encodeToString(hashCode, Base64.NO_WRAP)
 
-        verifyPinCode(VerifyPINCodeDTOReq(
+        val body = VerifyPINCodeDTOReq(
                 uid = faceArg.userID,
                 paymentID = paymentArg.paymentId,
                 pinCode = finalCode,
                 clientIP = paymentArg.clientIp
-        ))
+        )
+        verifyPinCode(body, paymentArg, faceArg)
     }
 
-    private fun verifyPinCode(req: VerifyPINCodeDTOReq) {
+    private fun verifyPinCode(req: VerifyPINCodeDTOReq, paymentArg: PaymentArg, faceArg: FaceArg) {
 
         PaymentRepository.ins.verifyPINCode(dataReq = req, listener = object : Api.ClientListener<PinArg> {
             override fun onSuccess(data: PinArg) {
-                onPinVerifySuccess(data)
+                onPinVerifySuccess(data, paymentArg, faceArg)
             }
 
             override fun onFailed(code: Int, message: String) {
@@ -58,8 +61,29 @@ class PinVM : BaseViewModel() {
         })
     }
 
-    private fun onPinVerifySuccess(data: PinArg) {
+    private fun onPinVerifySuccess(data: PinArg, paymentArg: PaymentArg, faceArg: FaceArg) {
         pinArg.postValue(data)
+        val body = PaymentDTOReq(
+
+        )
+        PaymentRepository.ins.payment(body, object : Api.ClientListener<PaymentDTOResp> {
+            override fun onSuccess(data: PaymentDTOResp) {
+
+                when {
+                    data.code == 0 -> {
+                        // show progress face paid
+                    }
+                    data.haveOTP && !data.formOtp.isNullOrEmpty() -> {
+                        //navigate napas
+                    }
+                    else -> {
+                        // failed
+                    }
+                }
+                paymentResult.postValue(true)
+            }
+
+        })
     }
 
     private fun onPinVerifyFailed(code: Int, message: String? = null) {
