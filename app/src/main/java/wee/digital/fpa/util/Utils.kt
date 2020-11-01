c9155615015daebb0250d3fd3238c070487790f0
KhanhNguyen <khanhnguyen@wee.vn>
2020-11-01 16:23:24 +0700

    update err

--- app/src/main/java/wee/digital/fpa/util/Utils.kt
+++ app/src/main/java/wee/digital/fpa/util/Utils.kt
@@ -79,86 +79,81 @@ object Utils {
     /**
      * set content Fail
      */
-    fun getContentFailFromCode(code: Int): DataContentFail {
-        var title = ""
-        var mess = ""
+    fun getContentFailFromCode(code: Int,block: (String,String)->Unit) {
         when (code) {
             ErrCode.BANK_UNKNOWN_ERROR -> {
-                title = "Thanh toán không thành công"
-                mess = "Tài khoản ngân hàng liên kết hiện tại không thể thực hiện thanh toán. Bạn Vui lòng thử lại"
+                block("Thanh toán không thành công",
+                        "Tài khoản ngân hàng liên kết hiện tại không thể thực hiện thanh toán. Bạn Vui lòng thử lại")
             }
 
             ErrCode.USER_BANK_ACC_NOT_FOUND -> {
-                title = "Thanh toán không thành công"
-                mess = "Không tìm thấy tài khoản ngân hàng đã liên kết"
+                block("Thanh toán không thành công",
+                        "Không tìm thấy tài khoản ngân hàng đã liên kết")
             }
 
             ErrCode.ACC_FACEPAY_LOCKED, ErrCode.ACC_LOCKED -> {
-                title = "Không thể thực hiện giao dịch"
-                mess = "Tài khoản Facepay của bạn hiện đang tạm khóa chức năng thanh toán."
+                block("Thanh toán không thành công",
+                        "Tài khoản Facepay của bạn hiện đang tạm khóa chức năng thanh toán.")
             }
 
             ErrCode.NOT_LINKED_BANK_ACC, ErrCode.BANK_ACC_NOT_FOUND -> {
-                title = "Tài khoản chưa liên kết ngân hàng"
-                mess = "Giao dịch không thể thực hiện do tài khoản của bạn chưa liên kết ngân hàng. Bạn vui lòng liên kết tài khoản trước khi thanh toán"
+                block("Tài khoản chưa liên kết ngân hàng",
+                        "Giao dịch không thể thực hiện do tài khoản của bạn chưa liên kết ngân hàng. Bạn vui lòng liên kết tài khoản trước khi thanh toán")
             }
 
             ErrCode.INSUFFICIENT_ACC_BALANCE -> {
-                title = "Số dư không đủ để thực hiện thanh toán"
-                mess = "Tài khoản liên kết hiện tại không đủ số dư để thực hiện giao dịch"
+                block("Số dư không đủ để thực hiện thanh toán",
+                        "Tài khoản liên kết hiện tại không đủ số dư để thực hiện giao dịch")
+
             }
 
             ErrCode.FACE_NOT_FOUND -> {
-                title = "Tài khoản không tồn tại"
-                mess = "Bạn vui lòng đăng ký tài khoản Facepay trước khi thực hiện thanh toán"
+                block("Tài khoản không tồn tại",
+                        "Bạn vui lòng đăng ký tài khoản Facepay trước khi thực hiện thanh toán")
             }
 
             ErrCode.MAX_PIN_CODE_VERIFICATION, ErrCode.WRONG_PIN_CODE -> {
-                title = "Quá số lần nhập mã PIN"
-                mess = "Bạn đã nhập sai mã PIN quá số lần cho phép, giao dịch không thể thực hiện."
+                block("Quá số lần nhập mã PIN",
+                        "Bạn đã nhập sai mã PIN quá số lần cho phép, giao dịch không thể thực hiện.")
             }
 
             ErrCode.USER_IS_OWNER -> {
-                title = "Thanh toán không thành công"
-                mess = "Chủ shop không thể thanh toán, bạn vui lòng thử lại"
+                block("Thanh toán không thành công","Chủ shop không thể thanh toán, bạn vui lòng thử lại")
             }
 
             ErrCode.TIMEOUT_ERROR -> {
-                title = "Hết thời gian thanh toán"
-                mess = "Giao dịch của bạn đã quá thời gian thanh toán. Bạn vui lòng thực hiện lại giao dịch."
+                block("Hết thời gian thanh toán","Giao dịch của bạn đã quá thời gian thanh toán. Bạn vui lòng thực hiện lại giao dịch.")
             }
 
             ErrCode.TRANS_EXCEED_LIMIT -> {
-                title = "Quá hạn mức giao dịch"
-                mess = "Giao dịch không thể thực hiện do số tiền yêu cầu thanh toán vượt quá hạn mức giao dịch của tài khoản."
+                block("Quá hạn mức giao dịch","Giao dịch không thể thực hiện do số tiền yêu cầu thanh toán vượt quá hạn mức giao dịch của tài khoản.")
+
             }
 
             ErrCode.DEVICE_DELETE -> {
-                title = "Thiết bị mất kết nối với hệ thống quản trị"
-                mess = "Bạn vui lòng kết nối lại thiết bị"
+                block("Thiết bị mất kết nối với hệ thống quản trị","Bạn vui lòng kết nối lại thiết bị")
+
             }
 
             ErrCode.MAX_FACE_VERIFICATION -> {
-                title = "Nhận diện khuôn mặt không thành công"
-                mess = "Bạn đã quá số lần nhận diện khuôn mặt cho phép, giao dịch không thể thực hiện."
+                block("Nhận diện khuôn mặt không thành công","Bạn đã quá số lần nhận diện khuôn mặt cho phép, giao dịch không thể thực hiện.")
+
             }
 
             ErrCode.FACE_PAY_ERROR -> {
-                title = "Thanh toán không thành công"
-                mess = "Phát hiện lỗi, Vui lòng thử lại"
+                block("Thanh toán không thành công","Phát hiện lỗi, Vui lòng thử lại")
+
             }
 
             ErrCode.BILL_PAID -> {
-                title = "Thanh toán không thành công"
-                mess = "Giao dịch của bạn đã được thanh toán trước đó"
+                block("Thanh toán không thành công","Giao dịch của bạn đã được thanh toán trước đó")
+
             }
 
             else -> {
-                title = "Thanh toán không thành công"
-                mess = "Phát hiện lỗi, Vui lòng thử lại"
+                block("Thanh toán không thành công","Phát hiện lỗi, Vui lòng thử lại")
             }
         }
-        return DataContentFail(title, mess)
     }
 
 }
