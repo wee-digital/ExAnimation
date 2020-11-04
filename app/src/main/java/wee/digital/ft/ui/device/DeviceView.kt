41cc9f73b281d61e3abecdb8d1486150cb3fc680
Huy Quoc Vu <huyquocvu.sg@gmail.com>
2020-11-04 13:32:11 +0700

    commit

--- app/src/main/java/wee/digital/ft/ui/device/DeviceView.kt
+++ app/src/main/java/wee/digital/ft/ui/device/DeviceView.kt
@@ -1,52 +1,15 @@
 package wee.digital.ft.ui.device
 
 import android.text.Editable
-import android.view.inputmethod.EditorInfo
 import com.google.gson.JsonObject
 import kotlinx.android.synthetic.main.device.*
 import wee.digital.ft.R
+import wee.digital.ft.shared.Config
 import wee.digital.ft.util.SimpleTextWatcher
 import wee.digital.library.extension.*
 
-class DeviceView(private val v: DeviceFragment) {
+class DeviceView(private val v: DeviceFragment) : SimpleTextWatcher {
 
-    private fun configDeviceNameText() {
-        v.deviceEditTextName.apply {
-            imeOptions = EditorInfo.IME_ACTION_DONE
-            maxLines = 1
-            isSingleLine = true
-            setEms(10)
-            addTextChangedListener(object : SimpleTextWatcher {
-
-                override fun afterTextChanged(editable: Editable?) {
-                    val s = text.toString()
-                    if (s == " ") {
-                        removeTextChangedListener(this)
-                        setText("")
-                        addTextChangedListener(this)
-                        return
-                    }
-                    if (s.length == 20 && s[19].toString() == " ") {
-                        removeTextChangedListener(this)
-                        val str = text.toString().removeRange(19, 20)
-                        setText(str)
-                        setSelection(text.toString().length)
-                        addTextChangedListener(this)
-                        return
-                    }
-                    if (s.length < 2 || s.length >= 19) return
-                    val spaceOne = s[s.length - 1].toString()
-                    val spaceTwo = s[s.length - 2].toString()
-                    if (spaceOne == " " && spaceTwo == " ") {
-                        removeTextChangedListener(this)
-                        setText(s.replace("  ", " "))
-                        setSelection(text.toString().length)
-                        addTextChangedListener(this)
-                    }
-                }
-            })
-        }
-    }
 
     private fun configTermText() {
         val sPolicy = string(R.string.device_policy).bold().color("#419DF2")
@@ -66,9 +29,9 @@ class DeviceView(private val v: DeviceFragment) {
 
 
     fun onViewInit() {
+        v.deviceEditTextName.filterChars(Config.DEVICE_NAME_FILTER)
         v.deviceEditTextName.requestFocus()
         v.deviceViewProgress.load(R.mipmap.img_progress_small)
-        configDeviceNameText()
         configTermText()
         v.addClickListener(v.deviceViewBack, v.deviceViewClose, v.deviceViewRegister)
         v.deviceEditTextName.requestFocus()
@@ -83,6 +46,12 @@ class DeviceView(private val v: DeviceFragment) {
     fun hideProgress() {
         v.deviceViewRegister.show()
         v.deviceViewProgress.hide()
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        v.deviceEditTextName.removeTextChangedListener(this)
+        v.deviceEditTextName.trimIndentText
+        v.deviceEditTextName.addTextChangedListener(this)
     }
 
 }
